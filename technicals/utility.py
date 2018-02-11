0634e94b99e09b21b9c50e4e9e47618447c298ed
Ryo Hayama <ryomarcher@gmail.com>
2018-02-11 09:52:51 -0500

    candlestick plot implemented using bokeh.

--- technicals/utility.py
+++ technicals/utility.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import bokeh
 import math
+from bokeh.plotting import figure, show, output_file
+from bokeh.io import output_notebook
 
 ### The following code assumes the data contains columns named time, open, high,
 ### low, and close
@@ -10,7 +11,7 @@ import math
 def get_period_data(subset):
     subset = subset.reset_index()
     time_ = subset['time'].max()
-    open_ = subset['open'].iloc[-1]
+    open_ = subset['open'].iloc[0]
     high_ = subset['high'].max()
     low_ = subset['low'].min()
     close_ = subset['close'].iloc[-1]
@@ -46,3 +47,35 @@ def aggregate_periods(data, period=20):
                                'close': closes})
     aggregated = aggregated[['time', 'open', 'high', 'low', 'close']].set_index('time')
     return aggregated
+
+def plot_candlestick(data, title_label=None):
+    '''
+    ### ***NEED to recalibrate the w parameter properly ####
+    '''
+
+    # Adopted from https://bokeh.pydata.org/en/latest/docs/gallery/candlestick.html
+    df = data
+
+    # Get boolean increase or decrease in the level for each row
+    inc = df.close > df.open
+    dec = df.open > df.close
+
+    # set bar width to 60% of time interval in milliseconds
+    time0 = data.index[0]
+    time1 = data.index[1]
+    time_interval = time1 - time0
+    w = 0.6 * time_interval.seconds * 1000
+
+    TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+
+    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=800, title=title_label)
+    p.xaxis.major_label_orientation = math.pi/4
+    p.grid.grid_line_alpha=0.3
+
+    p.segment(df.index, df.high, df.index, df.low, color="black")
+    p.vbar(df.index[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(df.index[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
+
+    output_notebook()#"candlestick.html", title="candlestick.py example")
+
+    show(p)
